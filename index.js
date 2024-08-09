646bc035b370a68313d55ea46f339a5f80909be3
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 12:55:48 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Declare a variable that stores an array of any 3 fictional people
+let fiction = ["Robin","Flash","Spiderman","Batman"]
+
+// Console log the first element of the array (using array name and index)
+console.log(fiction[0])
+// Console log the last element of the array (using array name and index)
+console.log(fiction[3])
+
+// Console log the last element of the array WITHOUT using the index -- use the array length instead
+console.log(fiction.length)
+
+// Change the second element of the array to be a different fictional character
+fiction[1] = "Kirby"
+
+// Stretch: try ADDING a new fictional character to the array
+
